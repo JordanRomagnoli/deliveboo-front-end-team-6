216a1ccdc6b320057d12fb47886ff95ae3e1fc29
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-04-05 22:55:39 +0200

    edit new function

--- src/components/RestaurantList/RestaurantListMain.vue
+++ src/components/RestaurantList/RestaurantListMain.vue
@@ -5,25 +5,34 @@
         data() {
             return { 
                 restaurants: [],
+                typologies: [],
                 currentPage: 1,  
                 lastPage: 1,
             }
         },
         created(){
-        this.getRestaurants(this.currentPage)
+        this.getRestaurants(this.currentPage);
+        this.getTypologies();
         },
         methods: {
         getRestaurants(page){
             Axios.get('http://127.0.0.1:8000/api/restaurant',{
                 params:{
-                page: page,
+                page: page
             }
             })
             .then(res =>{
-                console.log(res.data);
+                console.log(res.data,"ristorante");
                 this.restaurants = res.data.results.data;
                 this.currentPage = res.data.results.current_page;
                 this.lastPage = res.data.results.last_page;
+            });
+        },
+            getTypologies() {
+            Axios.get('http://127.0.0.1:8000/api/typology')
+            .then(res => {
+                console.log(res.data,"tipologia");
+                this.typologies = res.data.typologies; 
             });
             },
             prevPage() {
@@ -52,9 +61,14 @@
             <h2>
               {{ restaurant.company_name }}
             </h2> 
-            <li v-for="typologies in restaurant.typology" :key="typology.id">
+            <li v-for="typology in restaurant.typologies" :key="typology.id">
                 Ristorante: {{ typology.name }}
-            </li>       
+            </li>
+            <button class="specialbutton">
+                <router-link :to="{ name: 'restaurant', params: { slug: restaurant.slug } }" class="btn btn-primary">
+                    Vedi post completo 
+                </router-link>
+            </button>
         </div>
         <div class="sectionpage">
             <button @click="prevPage()">
