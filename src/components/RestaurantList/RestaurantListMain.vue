c0c098a9b65f16f6ee194cb4444631d40a7f59ab
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-04-05 22:17:29 +0200

    add sskeleton of restaurants page

--- src/components/RestaurantList/RestaurantListMain.vue
+++ src/components/RestaurantList/RestaurantListMain.vue
@@ -1,28 +1,80 @@
 <script>
-
     import Axios from 'axios';
 
     export default {
         data() {
             return { 
-                    
+                restaurants: [],
+                currentPage: 1,  
+                lastPage: 1,
             }
         },
         created(){
-            Axios.get('http://127.0.0.1:8000/api/restaurant')
-            .then(res =>{
-                console.log(res);
+        this.getRestaurants(this.currentPage)
+        },
+        methods: {
+        getRestaurants(page){
+            Axios.get('http://127.0.0.1:8000/api/restaurant',{
+                params:{
+                page: page,
+            }
             })
+            .then(res =>{
+                console.log(res.data);
+                this.restaurants = res.data.results.data;
+                this.currentPage = res.data.results.current_page;
+                this.lastPage = res.data.results.last_page;
+            });
+            },
+            prevPage() {
+                if (this.currentPage > 1) {
+                this.getRestaurants(this.currentPage - 1)
+                }
+            },
+            nextPage() {
+                if (this.currentPage < this.lastPage) {
+                this.getRestaurants(this.currentPage + 1)
+                }
+            }
         }
     }
 </script>
 
 <template>
-    <main class="main-title">
+    <!-- <main class="main-title">
         
-    </main>
+    </main> -->
+    <div class="mycontainer">
+        <div v-for="restaurant in restaurants" :key="restaurant.id" class="myrestaurantcard">
+            <div v-if="restaurant.img !=null">
+              <img :src="'http://127.0.0.1:8000/storage/' + restaurant.img" :alt="restaurant.company_name">
+            </div>
+            <h2>
+              {{ restaurant.company_name }}
+            </h2> 
+            <li v-for="typologies in restaurant.typology" :key="typology.id">
+                Ristorante: {{ typology.name }}
+            </li>       
+        </div>
+        <div class="sectionpage">
+            <button @click="prevPage()">
+                Precedente
+            </button>
+            <button @click="nextPage()">
+                Successivo
+            </button>
+        </div>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
     @use '../../assets/scss/footer.scss' as *;
+    .mycontainer{
+        max-width: 1200px;
+        margin: 0 auto;
+        width: 100%;
+        padding: 30px;
+    }
+
 </style>
