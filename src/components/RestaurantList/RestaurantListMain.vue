fce45d9ff05bf212bdf0fa99502e24a72e2b4596
MirkoCifelli <ciffo4@hotmail.it>
2024-04-06 17:48:44 +0200

    Change path axios img , card style

--- src/components/RestaurantList/RestaurantListMain.vue
+++ src/components/RestaurantList/RestaurantListMain.vue
@@ -1,70 +1,78 @@
 <script>
-    import Axios from 'axios';
+import Axios from "axios";
 
-    export default {
-        data() {
-            return { 
-                restaurants: [],
-                typologies: [],
-                currentPage: 1,  
-                lastPage: 1,
-            }
-        },
-        created(){
+export default {
+    data() {
+        return {
+            restaurants: [],
+            typologies: [],
+            currentPage: 1,
+            lastPage: 1,
+        };
+    },
+    created() {
         this.getRestaurants(this.currentPage);
         this.getTypologies();
-        },
-        methods: {
-        getRestaurants(page){
-            Axios.get('http://127.0.0.1:8000/api/restaurant',{
-                params:{
-                page: page
-            }
-            })
-            .then(res =>{
-                console.log(res.data,"ristorante");
+    },
+    methods: {
+        getRestaurants(page) {
+            Axios.get("http://127.0.0.1:8000/api/restaurant", {
+                params: {
+                    page: page,
+                },
+            }).then((res) => {
+                console.log(res.data, "ristorante");
                 this.restaurants = res.data.results.data;
                 this.currentPage = res.data.results.current_page;
                 this.lastPage = res.data.results.last_page;
             });
         },
-            getTypologies() {
-            Axios.get('http://127.0.0.1:8000/api/typology')
-            .then(res => {
-                console.log(res.data,"tipologia");
-                this.typologies = res.data.typologies; 
+        getTypologies() {
+            Axios.get("http://127.0.0.1:8000/api/typology").then((res) => {
+                console.log(res.data, "tipologia");
+                this.typologies = res.data.typologies;
             });
-            },
-            prevPage() {
-                if (this.currentPage > 1) {
-                this.getRestaurants(this.currentPage - 1)
-                }
-            },
-            nextPage() {
-                if (this.currentPage < this.lastPage) {
-                this.getRestaurants(this.currentPage + 1)
-                }
+        },
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.getRestaurants(this.currentPage - 1);
             }
-        }
-    }
+        },
+        nextPage() {
+            if (this.currentPage < this.lastPage) {
+                this.getRestaurants(this.currentPage + 1);
+            }
+        },
+    },
+};
 </script>
 
 <template>
-    
     <div class="mycontainer">
-        <div class="p-4">
-            carosello tipologie img
-        </div>
+        <div class="p-4">carosello tipologie img</div>
         <div class="containerflex">
-            <div v-for="restaurant in restaurants" :key="restaurant.id" class="myrestaurantcard">
-                <div v-if="restaurant.img !=null">
-                    <img :src="'http://127.0.0.1:8000/storage/' + restaurant.img" :alt="restaurant.company_name">
+            <div
+                v-for="restaurant in restaurants"
+                :key="restaurant.id"
+                class="myrestaurantcard"
+            >
+                <div v-if="restaurant.img != null" class="myrestaurantimg">
+                    <img
+                        :src="
+                            'http://127.0.0.1:8000/storage/images/' +
+                            restaurant.img
+                        "
+                        :alt="restaurant.company_name"
+                    />
                 </div>
                 <div class="myrestaurantcardbody p-4">
-                    <h2>
+                    <h2 class="text-h2">
                         {{ restaurant.company_name }}
-                    </h2> 
-                    <h6 v-for="typology in restaurant.typologies" :key="typology.id">
+                    </h2>
+                    <h6
+                        v-for="typology in restaurant.typologies"
+                        :key="typology.id"
+                    >
                         Ristorante: {{ typology.name }}
                     </h6>
                     <!-- <button class="specialbutton">
@@ -75,81 +83,82 @@
                 </div>
             </div>
             <div class="button p-4">
-                <button @click="prevPage()">
-                    Precedente
-                </button>
+                <button @click="prevPage()">Precedente</button>
             </div>
             <div class="button p-4">
-                <button @click="nextPage()">
-                    Successivo
-                </button>
+                <button @click="nextPage()">Successivo</button>
             </div>
         </div>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
-    @use '../../assets/scss/footer.scss' as *;
+@use "../../assets/scss/footer.scss" as *;
 
-    .mycontainer{
-        max-width: 1200px;
-        margin: 0 auto;
+.mycontainer {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 100%;
+    padding: 40px;
+}
+
+.containerflex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.myrestaurantcard {
+    height: 200px;
+    width: calc(100% / 3 - 20px);
+    margin: 10px;
+    position: relative;
+    box-shadow: 10px 10px 5px rgba(211, 211, 211, 0.613);
+    border-radius: 30px; /* Imposta il raggio dell'arrotondamento */
+    overflow: hidden; /* Assicura che l'immagine non superi i bordi arrotondati */
+    img {
         width: 100%;
-        padding: 40px;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
     }
-
-    .containerflex{
-        display: flex;
-        justify-content: space-between;
-        align-items: center; 
-        flex-wrap:wrap;
+    .myrestaurantimg {
+        width: 100%;
+        height: 100%;
+        opacity: 0.9;
+        filter: blur(1px);
     }
-
-    .myrestaurantcard{
-        // border: 2px solid black;
-        height: 300px; 
-        width: calc(100% / 3 - 20px);
-        padding: 10px;
-        margin: 10px;
-        border-radius: 20px;
-        position: relative;
-        box-shadow: 10px 10px 5px rgba(211, 211, 211, 0.613);
-
-        &>img{
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: center;
-        }
-
-        .myrestaurantcardbody{
+    .myrestaurantcardbody {
         position: absolute;
         bottom: 0;
+        width: 100%;
+        height: 30%;
+        padding: 0 !important;
+        .text-h2 {
+            text-shadow: 2px 2px 4px rgba(211, 211, 211, 0.613);
+            color: white;
+            text-align: center;
         }
-
-        
     }
-        button {
-                background-color: #3498DB;
-                border: none;
-                color: white;
-                padding: 7px 10px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 13px;
-                margin: 4px 2px;
-                transition-duration: 0.4s;
-                cursor: pointer;
-                }
+}
+button {
+    background-color: #3498db;
+    border: none;
+    color: white;
+    padding: 7px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 13px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
 
-
-                button:hover {
-                background-color: white;
-                color: #3498DB;
-                border:1px solid #3498DB;
-                }
-
-
+button:hover {
+    background-color: white;
+    color: #3498db;
+    border: 1px solid #3498db;
+}
 </style>
