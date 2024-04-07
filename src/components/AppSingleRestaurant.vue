cfa0d0c79fd93ba13e73208c90b9d4875c3f7153
martinaapalazzi <martinaapalazzi@gmail.com>
2024-04-07 13:51:06 +0200

    created single restaurant component

---
+++ src/components/AppSingleRestaurant.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        data() {
+            return { 
+                restaurant: null,
+            }
+        },
+        methods: {
+            getSingleRestaurant() {
+                axios.get('http://127.0.0.1:8000/api/restaurant'+ this.$route.params.slug)
+                    .then(response => {
+                        console.log(response.data);
+                    }
+                )
+            },
+        }
+    }
+    </script>
+
+<template>
+    <section>
+        <div class="restaurant-container">
+            
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/main.scss' as *;
+</style>
