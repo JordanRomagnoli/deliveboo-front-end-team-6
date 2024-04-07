d13d3ef6f4c3f37a5ded6f463d91b7465598dfe4
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-07 11:37:59 +0200

    UPDATED homepage

--- src/components/Home/TypologiesCarousell.vue
+++ src/components/Home/TypologiesCarousell.vue
@@ -6,11 +6,16 @@
     export default {
         data() {
             return {
-                store,               
+                store,      
             }
         },
         methods:{
 
+            selectTypology(typology){
+
+                this.store.selectedTypology.push(typology);
+                console.log(this.store.selectedTypology);
+            },
 
         },
         components:{
@@ -33,13 +38,13 @@
 <template>
     
     <div class="container-tipology"> 
-        <div v-for="(elem, i) in store.typologies" class="single-typology">
+        <div class="single-typology" v-for="index in 5" :key="index">
             <h3>
-                {{ elem.name }}
+                <!-- {{ elem.name }} -->
+                {{store.typologies[index - 1].name}}
             </h3>
-
-            <div class="img-container">
-                <img :src="'http://127.0.0.1:8000/storage/images/' + elem.img" alt="">
+            <div @click="selectTypology(store.typologies[index - 1].name)" class="img-container">
+                <img :src="'http://127.0.0.1:8000/storage/images/' + store.typologies[index - 1].img" alt="">
             </div>
         </div>
     </div>
@@ -47,5 +52,47 @@
 </template>
 
 <style lang="scss" scoped>
-    
+    .container-tipology{
+
+        width: 60%;
+        max-width: 60%;
+        display: flex;
+        position: absolute;
+        margin: 0 auto;
+        bottom: - 170px;
+        left: 0;
+        right: 0;
+        z-index: 3;
+        .single-typology{
+
+            width: calc((100% / 5) - 16px);
+            margin: 0px 8px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            h3{
+                font-size: 1.2rem;
+                color: white;
+                margin-bottom: 16px;
+            }
+            
+
+            .img-container{
+
+                width: 230px;
+                height: 230px;
+                //border: 1px solid white;
+                border-radius: 20px;
+                overflow: hidden;
+                cursor: pointer;
+                img{
+                    height: 100%;
+                    object-fit: cover;
+                    width: 100%;
+                    filter: grayscale(0.5);
+                    
+                }
+            }            
+        }
+    }
 </style>
