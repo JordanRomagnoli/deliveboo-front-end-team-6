c596795f3e4037be24c22420b97f0bcf5e01f3c2
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-04-09 11:40:54 +0200

    ADD cart

--- src/store.js
+++ src/store.js
@@ -5,4 +5,10 @@ export const store = reactive({
     selectedTypology: [],
     InputHome: '',
     currentRestaurants: [],
+    selectedDishes: [],
+    /*
+        Se l'utente prova ad aggiungere un piatto di un altro ristorante,
+        fare il controllo sul ristorante dei piatti già presenti nell'array
+    */
+    totalPrice: null,
 });
